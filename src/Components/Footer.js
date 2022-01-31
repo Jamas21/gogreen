4e6aee4eece6d0542692f9c69e6b948ef94835e9
Jozsa Tamas <jtms021@gmail.com>
2022-01-31 19:04:00 +0200

    first commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './Footer.css';
+
+
+
+// This piece of code will render our website footer
+const Footer = () => {
+    return(
+        <div className="App-frame">
+            <div className="App-footer">
+                <h1>Footer</h1>
+            </div>
+        </div>
+        
+    )
+}
+
+export default Footer;
---
+++ src/Components/Main.js
@@ -0,0 +1,30 @@
+import React from "react";
+import './Main.css';
+
+
+// This piece of code will render our website looks
+const Main = () => {
+    return(
+        <div className="App-frame">
+            <div className="App-main">
+                <h1>This will be main content.</h1>
+                <h1>This will be main content.</h1>
+                <h1>This will be main content.</h1>
+                <h1>This will be main content.</h1>
+                <h1>This will be main content.</h1>
+                <h1>This will be main content.</h1>
+                <h1>This will be main content.</h1>
+                <h1>This will be main content.</h1>
+                <h1>This will be main content.</h1>
+                <h1>This will be main content.</h1>
+                <h1>This will be main content.</h1>
+                <h1>This will be main content.</h1>
+                <h1>This will be main content.</h1>
+              
+            </div>
+        </div>
+        
+    )
+}
+
+export default Main;
